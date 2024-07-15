7eafc4ec4f436559a1204836ba37afce0b32dffe
Saban Celik <aban.elik27@gmail.com>
2024-07-15 23:38:58 +0300

    I created about page for about bar

--- pages/about.php
+++ pages/about.php
@@ -7,12 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/about.css">
-<link href="https://fonts.googleapis.com/css2?family=Playwrite+CL:wght@400&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Playwrite+CL:wght@400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
     <div class="main-about-div">
+        <!-- About One Content -->
         <div class="about-one-content-all">
             <div class="about-one-content container">
                 <h2>Foto X Kimdir?</h2>
@@ -22,6 +22,8 @@
                 </div>
             </div>
         </div>
+
+        <!-- About Two Content -->
         <div class="about-two-content-all">
             <div class="about-two-content-half black"></div>
             <div class="about-two-content-half white"></div>
@@ -29,6 +31,8 @@
                 <img src="img/aboutFirstPhoto.jpg" alt="About Image">
             </div>
         </div>
+
+        <!-- About Three Content -->
         <div class="about-three-content-all">
             <div class="about-three-content container">
                 <img src="img/aboutPhotoTwo.jpg" alt="Story Image">
@@ -38,6 +42,7 @@
                     <p>Mimari olarak görülmemiş yerleri fotoğraflamayı ve farklı kültürleri tanımayı çok seven biriydim. Bu yüzden İstanbul’un en güzel semtleri değil, en aykırı semtlerinde fotoğraf çekmek daha çok hoşuma gidiyordu. İlk yıllarda Tarlabaşı, Dolapdere, Hacı Hüsrev, Kasımpaşa gibi İstanbul’un en belalı semtlerinde sürekli fotoğraf çekiyordum. Bu semtlerde çektiğim fotoğraflarla birçok yerel ve ulusal ödül aldım. Ancak, bir üniversite öğrencisi için bu hobi oldukça pahalıydı. Satın almam gereken ekipmanlar çok pahalıydı ve istediğim fotoğrafları çekebilmek için bu ekipmanları almak zorundaydım. Artık gerçekten bu işten para kazanmak zorundaydım ve kendimi tam anlamıyla hazır hissettiğimde, internet sitemi kurup iyi bir portfolyo oluşturup düğün fotoğrafçılığı kariyerime başladım.</p>
                 </div>
             </div>
+
             <div class="about-three-content container">
                 <div class="about-three-text">
                     <h2>Fotoğraf Sanatı ve Sosyal Medya Ajansı</h2>
@@ -45,6 +50,38 @@
                     <p>Geçmişteki dijital reklamcılık alanındaki tecrübelerimle birlikte, birkaç müşterimden gelen teklifler doğrultusunda, mevcut ekibimle birlikte birçok prodüksiyon işine imza attık. Bu sürecin sonunda PixelWorks sosyal medya ajansımızı kurduk. Şu an, fotoğrafçılık ve dijital pazarlama alanında yetiştirdiğim güçlü ekibim ile birlikte müşterilerimize en iyi hizmeti sunmaktan gurur duyuyoruz.</p>
                 </div>
                 <img src="img/aboutPhotoOne.jpg" alt="Agency Image">
+            </div>
+        </div>
+
+        <!-- About Four Content -->
+        <div class="about-four-content-all">
+            <div class="about-four-content container">
+                <h2>Çalışmalarımı Yönlendiren Vazgeçilmez Değerler</h2>
+                <div class="line"></div>
+                <p>Her müşterimizin fotoğraf çekimi sürecini unutulmaz kılmak için kişiye özel bir müşteri deneyimi sunmaya odaklanıyoruz.</p>
+                <div class="row">
+                    <div class="col-md-4 value-card">
+                        <h3>01<br/>Mükemmeliyetçilik</h3>
+                        <p>Sürekli öğrenim ve gelişme üzerine vizyonumuzla kendimizi ve hizmetlerimizi sürekli olarak iyileştiriyor ve müşteri memnuniyeti ve yüksek kalite standartlarına olan bağlılığımızla mükemmeliğe ulaşmayı hedefliyoruz.</p>
+                    </div>
+                    <div class="col-md-4 value-card">
+                        <h3>02<br/>Kalite</h3>
+                        <p>Her aşamada detaylara odaklanarak, estetik ve teknik mükemmeliği bir araya getiriyor ve müşterilerimize yüksek kaliteli ve özgün fotoğrafçılık hizmetleri profesyonel bir şekilde sunmaktan gurur duyuyoruz.</p>
+                    </div>
+                    <div class="col-md-4 value-card">
+                        <h3>03<br/>Yaratıcılık</h3>
+                        <p>Her müşterinin benzersiz hikayesini yaratıcılığımızla profesyonel bir şekilde ifade ederek, kişisel bir dokunuş katıyor, yaratıcılığı destekleyerek sıradışı ve etkileyici çekimler yapmaya odaklanıyoruz.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- About Five Content -->
+        <div class="about-five-content-all">
+            <div class="about-five-content container">
+                <h2>PROFESYONEL FOTOĞRAFÇILIK KONUSUNDA YARDIMA MI İHTİYACINIZ VAR?</h2>
+                <p>İşinize, Anılarınıza Profesyonel Bir Dokunuş Katmak İstiyorsanız, Bizimle Çalışabilirsiniz.</p>
+                <a href="index.php?page=contact" class="btn btn-primary">İletişime Geç</a>
             </div>
         </div>
     </div>
